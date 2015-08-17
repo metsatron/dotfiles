2592f492078f6ca355f0776ae8cb7da3e8694123
metsarono <aero_glass@hotmail.com>
2015-08-17 11:55:27 +0100

    Added files from Unity

---
+++ unity/.config/fish/config.fish
@@ -0,0 +1,30 @@
+# Path to your oh-my-fish.
+set fish_path $HOME/.oh-my-fish
+
+# Theme
+# set fish_theme robbyrussell
+
+# All built-in plugins can be found at ~/.oh-my-fish/plugins/
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Enable plugins by adding their name separated by a space to the line below.
+set fish_plugins tmux archlinux mc sublime vi-mode
+
+# Path to your custom folder (default path is ~/.oh-my-fish/custom)
+#set fish_custom $HOME/dotfiles/oh-my-fish
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
+
+set -x EDITOR vim
+
+set fish_function_path $fish_function_path "$HOME/.local/lib/python2.7/site-packages/powerline/bindings/fish"
+powerline-setup
+
+set -gx PATH $PATH /home/tiago/.gem/ruby/2.2.0/bin
+
+set -U fish_greeting ""
+
+if test -f $HOME/.local/lib/python2.7/site-packages/powerline/bindings/tmux/powerline.conf
+      tmux source "$HOME/.local/lib/python2.7/site-packages/powerline/bindings/tmux/powerline.conf"
+end
+
