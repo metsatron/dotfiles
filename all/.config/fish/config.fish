24bf24752eca21840692c64220f3e2e7b1f69d36
metsarono <aero_glass@hotmail.com>
2018-08-13 19:24:10 +0200

    Added new fish and nvim configs. New fonts.

--- all/.config/fish/config.fish
+++ all/.config/fish/config.fish
@@ -1,24 +1,24 @@
 # Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
+# set fish_path $HOME/.oh-my-fish
 
 # Path to your custom folder (default path is ~/.oh-my-fish/custom)
 # set fish_custom $HOME/dotfiles/oh-my-fish
 
 # Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
+# . $fish_path/init.fish
 
 # Custom plugins and themes may be added to ~/.oh-my-fish/custom
 # Plugins and themes can be found at https://github.com/oh-my-fish/
-Theme 'bobthefish'
-Plugin 'theme'
-Plugin 'tmux'
-Plugin 'mc'
-Plugin 'sublime'
-Plugin 'vi-mode'
+# Theme 'bobthefish'
+# Plugin 'theme'
+# Plugin 'tmux'
+# Plugin 'mc'
+# Plugin 'sublime'
+# Plugin 'vi-mode'
 
 set -gx TERM xterm-256color
 set -gx EDITOR vim
-set -gx PATH $PATH /home/tiago/.gem/ruby/2.2.0/bin
+# set -gx PATH $PATH $HOME/.gem/ruby/2.2.0/bin
 set -U fish_greeting ""
 
 #set fish_function_path $fish_function_path "$HOME/.local/lib/python2.7/site-#packages/powerline/bindings/fish"
@@ -30,7 +30,4 @@ else if test -f /usr/lib/python2.7/site-packages/powerline/bindings/tmux/powerli
       tmux source "/usr/lib/python2.7/site-packages/powerline/bindings/tmux/powerline.conf"
 else if test -f /usr/local/lib/python2.7/site-packages/powerline/bindings/tmux/powerline.conf
       tmux source "/usr/local/lib/python2.7/site-packages/powerline/bindings/tmux/powerline.conf"
-end
-if test -f source /Users/tiago/.iterm2_shell_integration.fish
-    source /Users/tiago/.iterm2_shell_integration.fish
 end
