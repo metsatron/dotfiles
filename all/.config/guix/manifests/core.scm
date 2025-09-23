4d164f8ea9036eacba7ad0f8ea602786febf4e0e
metsarono <aero_glass@hotmail.com>
2025-09-24 06:35:18 +1000

    RSM GSS Guix & Flatpak integration

--- all/.config/guix/manifests/core.scm
+++ all/.config/guix/manifests/core.scm
@@ -3,17 +3,20 @@
 
 
 ;; [[file:../../../../guix.org::*User profile manifests][User profile manifests:1]]
-;; Core userland that is safe on a foreign distro
 (specifications->manifest
  '(
-   "git"
-   "htop"
-   "ripgrep"
-   "fd"
-   "jq"
-   "direnv"
-   "btop"
-   "python"
-   "node"   ;; small Node for tooling, adjust if you prefer system Node
+   "git" "htop" "ripgrep" "fd" "jq" "direnv" "btop"
+   "python" "node"
+   "glibc-locales"   ; for UTF-8 locales
+   "nss-certs"       ; TLS certs for HTTPS
+   "emacs"           ; or "emacs-pgtk" if you prefer pgtk
+   "emacs-vterm"     ; native vterm module for Emacs
+   "libvterm"
+   "neovim"
+   "lsd" "ranger"
+   "python-numpy" "python-sympy" "python-coloredlogs" "python-humanfriendly" "python-send2trash" "python-websockets" "python-unidecode" "python-wheel"
+   "moreutils"
+   "gimp" "inkscape"
+   "appmenu-gtk-module"
    ))
 ;; User profile manifests:1 ends here
