deb2a1701d420168c758a7854dd22acb9ef2bda1
metsarono <aero_glass@hotmail.com>
2025-09-24 06:35:18 +1000

    RSM GSS Guix & Flatpak integration

--- all/.config/guix/channels.scm
+++ all/.config/guix/channels.scm
@@ -3,17 +3,14 @@
 
 
 ;; [[file:../../../guix.org::*Channel pin, reproducible by time-machine if desired][Channel pin, reproducible by time-machine if desired:1]]
-;; Guix channels for Mètsàtron
+;; Guix channels for Mètsàtron (authenticated)
 (list
   (channel
     (name 'guix)
     (url "https://git.savannah.gnu.org/git/guix.git")
-    ;; Optional pin to a commit for stable rollouts. Uncomment to lock.
-    ;; (commit "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-    )
-  ;; Example community channel, disabled by default
-  ;; (channel
-  ;;   (name 'nonguix)
-  ;;   (url "https://gitlab.com/nonguix/nonguix.git"))
-)
+    (introduction
+      (make-channel-introduction
+        "9edb3f66fd807b096b48283debdcddccfea34bad"
+        (openpgp-fingerprint
+         "BBB0 2DDF 2A54 27B3 78B2 6E33 4B04 82B6 6299 CBB6")))))
 ;; Channel pin, reproducible by time-machine if desired:1 ends here
